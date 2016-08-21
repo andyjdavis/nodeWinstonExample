7ac04800696e77d7d94b477e8d239f03031e6cc7
Andrew Davis <andrew.affinity@gmail.com>
2016-08-21 20:56:41 +0800

    first commit

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import winstonTest from '../lib';
+
+describe('winston-test', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
